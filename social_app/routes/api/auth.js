e6e5bfcc53fed390c26ca5e1bcbc75eeb618b4d1
tuba-harmain <tubaharmain1211@gmail.com>
2022-03-23 02:21:39 +0530

    update folder

--- social_app/routes/api/auth.js
+++ social_app/routes/api/auth.js
@@ -1,6 +1,77 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcryptjs');
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => res.send ('Auth Route'));
+const User = require('../../models/User');
+
+router.get('/', auth, async (req, res) => {
+    try{
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    }catch(err){
+        console.erroe(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+router.post('/',
+[
+    body('email', 'Please enter a valid Email')
+    .isEmail(),
+
+    body('password', 'Password is required')
+    .exists()
+], 
+async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password} = req.body;
+
+    try{
+        // See if User exists
+        let user = await User.findOne({email});
+
+        if(!user) {
+            return res.status(400).json([{ msg : "Invalid Credentials!" }]);
+        }
+
+        // Checking Password
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch){
+            return res.status(400).json([{ msg : "Invalid Credentials!" }]);
+        }
+
+        // Return jsonwebtoken
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload, 
+            config.get("jwttoken"),
+            {expiresIn: 3600000 }, 
+            (err, token) => {
+                if(err) throw err;
+                res.json({ token });
+            });
+
+    } catch(err) { 
+        console.error(err.message);
+        res.status(500).send('Serve Error')
+    }
+
+});
+
+
 
 module.exports = router;
--- social_app/routes/api/users.js
+++ social_app/routes/api/users.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { body, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
 
 router.post('/',[
     body('name', 'Name is required')
@@ -13,13 +19,64 @@ router.post('/',[
     body('password', 'Please enter  password with 6 or more chars')
     .isLength({ min: 6 })
 ], 
-(req, res) => {
+async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send ('User Route');
+    const { name, email, password} = req.body;
+
+    try{
+        // See if User exists
+        let user = await User.findOne({email});
+
+        if(user) {
+            return res.status(400).json([{ msg : "User already exists!" }]);
+        }
+
+        // Get Users Gravator
+        const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        })
+
+        user = new User ({
+            name,
+            email,
+            avatar,
+            password
+        });
+
+        // Encrypt Password
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        // Return jsonwebtoken
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload, 
+            config.get("jwttoken"),
+            {expiresIn: 3600000 }, 
+            (err, token) => {
+                if(err) throw err;
+                res.json({ token });
+            });
+
+    } catch(err) { 
+        console.error(err.message);
+        res.status(500).send('Serve Error')
+    }
+
 });
 
 module.exports = router;

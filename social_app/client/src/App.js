2395980d0758d7c9d488f00eed0d17d82c1b43df
tuba-harmain <tubaharmain1211@gmail.com>
2022-06-20 16:56:02 +0530

    Not Found page

--- social_app/client/src/App.js
+++ social_app/client/src/App.js
@@ -14,6 +14,7 @@ import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts';
 import Post from './components/post/Post';
+import NotFound from './components/layout/NotFound';
 import PrivateRoute from './components/routing/PrivateRoute';
 //Redux
 import { Provider } from 'react-redux';
@@ -51,6 +52,7 @@ const  App = () => {
            <Route path="add-education" element = {<PrivateRoute component={AddEducation} />} />
            <Route path="posts" element = {<PrivateRoute component={Posts} />} />
            <Route path="posts/:id" element = {<PrivateRoute component={Post} />} />
+           <Route path="/*" element={<NotFound />} />
       </Routes>
        
     </Router>
--- social_app/client/src/components/layout/Alert.js
+++ social_app/client/src/components/layout/Alert.js
@@ -2,11 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
-const Alert = ({ alerts }) => alerts !== null && alerts.length > 0 && alerts.map( alert => (
-    <div key= {alert.id} className={`alert alert-${alert.alertType}`}>
-        { alert.msg }
+const Alert = ({ alerts }) => (
+    <div className="alert-wrapper">
+      {alerts.map((alert) => (
+        <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+          {alert.msg}
+        </div>
+      ))}
     </div>
-));
+  );
 
 Alert.propTypes = {
     alerts: PropTypes.array.isRequired

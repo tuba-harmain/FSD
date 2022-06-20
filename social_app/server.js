05f21f70122eb4df6ff49dc299d6fc952a316195
tuba-harmain <tubaharmain1211@gmail.com>
2022-06-20 15:44:06 +0530

    heroku production
    export default function FirstPost() {
      return <h1>First Pos

--- social_app/server.js
+++ social_app/server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const userRoute = require('./routes/api/users');
-const authRoute = require('./routes/api/auth');
-const postsRoute = require('./routes/api/posts');
-const profileRoute = require('./routes/api/profile');
+const path = require('path');
 
 const app = express();
 
@@ -13,12 +10,20 @@ connectDB();
 //Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running')); 
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
-app.use('/api/users', userRoute);
-app.use('/api/auth',  authRoute);
-app.use('/api/posts', postsRoute);
-app.use('/api/profile', profileRoute);
+//Serve Static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html' ))
+});
+}
 
 const PORT  = process.env.PORT || 5000;
 
